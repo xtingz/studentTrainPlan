9e2f9e57361b693bd23671390971cf6d4a616209
Ting Baby <Emily@xtingz.vip>
2020-05-17 20:43:29 +0800

    最终提交版
    小改动

--- errors.py
+++ errors.py
@@ -6,7 +6,6 @@ from main import app
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
